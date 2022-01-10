8be2b9d1156f25164d958295efc67aa9bca60e90
alfredochs <chanta.alfredo@gmail.com>
2022-01-10 18:12:33 +0100

    seeder

--- database/migrations/2022_01_10_164628_create_travels_table.php
+++ database/migrations/2022_01_10_164628_create_travels_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTripsTable extends Migration
+class CreateTravelsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateTripsTable extends Migration
      */
     public function up()
     {
-        Schema::create('trips', function (Blueprint $table) {
+        Schema::create('travels', function (Blueprint $table) {
             $table->id();
-            $table->string('destination');
-            $table->date('departure');
-            // $table->date('departure');
-            // $table->number('price');
+            $table->string('title');
+            $table->string('description');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateTripsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('trips');
+        Schema::dropIfExists('travels');
     }
 }
